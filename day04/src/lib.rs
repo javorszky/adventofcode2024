076e7d520ccf4541140828e91146c73c13ce2e7b
Gabor Javorszky <gabor@javorszky.co.uk>
2024-12-04 13:59:40 +0000

    Day 4 part 2

--- day04/src/lib.rs
+++ day04/src/lib.rs
@@ -4,11 +4,15 @@ use std::collections::HashMap;
 type Coordinate = (i32, i32);
 
 const CHECKMAS: &str = "MAS";
-const BAD_CHAR : &char = &'.';
+const BAD_CHAR: &char = &'.';
+const MS: &str = "MS";
+const SM: &str = "SM";
+
 
 pub struct Day04 {
     word_grid: HashMap<Coordinate, char>,
-    x_list: Vec<Coordinate>
+    x_list: Vec<Coordinate>,
+    a_list: Vec<Coordinate>,
 }
 
 enum Direction {
@@ -29,12 +33,20 @@ pub fn part1() -> String{
     format!("The word XMAS can be found {} times.", d4.how_many_xmas())
 }
 
+pub fn part2() -> String{
+    let contents = include_str!("../input.txt");
+    let d4 = Day04::new(contents);
+
+    format!("The word XMAS can be found {} times.", d4.how_many_x_mas_part2())
+}
+
 impl Day04 {
     pub fn new(input: &str) -> Day04 {
         let lines = input.trim().lines().collect::<Vec<&str>>();
 
         let mut word_grid = HashMap::<(i32, i32), char>::new();
         let mut x_list = Vec::<Coordinate>::new();
+        let mut a_list = Vec::<Coordinate>::new();
 
         for (height, line) in lines.iter().enumerate() {
             for (width, character) in line.chars().enumerate() {
@@ -42,12 +54,17 @@ impl Day04 {
                 if character == 'X' {
                     x_list.push((width as i32, height as i32));
                 }
+
+                if character == 'A' {
+                    a_list.push((width as i32, height as i32));
+                }
             }
         }
 
         Day04 {
             word_grid,
-            x_list
+            x_list,
+            a_list,
         }
     }
 
@@ -68,13 +85,26 @@ impl Day04 {
         // let's iterate through the Xes
         for coordinate in self.x_list.iter() {
             for direction in &all_directions {
-                if self.is_this_xmas(generate_coordinates(coordinate, direction)) {
+                if self.is_this_xmas(generate_coordinates_part1(coordinate, direction)) {
                     xmases += 1;
                 }
             }
         }
 
         xmases
+    }
+
+    pub fn how_many_x_mas_part2(&self) -> i32 {
+        let mut x_mas = 0;
+        // for all A character
+        for a in self.a_list.iter() {
+            let coords = generate_coordinate_pairs_part2(a);
+            if self.is_this_x_mas(&coords) {
+                x_mas += 1;
+            }
+        }
+
+        x_mas
     }
 
     /// Grab the characters at the vecs, and compare if the resulting parts is "MAS". We started at
@@ -88,9 +118,28 @@ impl Day04 {
 
         mas == CHECKMAS
     }
+
+    /// Used with the coordinates we get from generate coordinates part 2, this checks whether
+    /// letters surrounding A are MS or SM. To be used with a coordinate pair for either + or x
+    /// orientation.
+    fn is_this_x_mas(&self, coords: &[[Coordinate; 2]; 2]) -> bool {
+        for coord_pair in coords.iter() {
+            let mut mas = "".to_string();
+
+            for coordinate in coord_pair {
+                mas.push(self.word_grid.get(coordinate).unwrap_or(BAD_CHAR).to_owned());
+            }
+
+            if mas != MS && mas != SM {
+                return false;
+            }
+        }
+
+        true
+    }
 }
 
-fn generate_coordinates(origin: &Coordinate, direction: &Direction) -> Vec<Coordinate> {
+fn generate_coordinates_part1(origin: &Coordinate, direction: &Direction) -> Vec<Coordinate> {
     match direction {
         Direction::North => {
             vec![
@@ -151,6 +200,21 @@ fn generate_coordinates(origin: &Coordinate, direction: &Direction) -> Vec<Coord
     }
 }
 
+/// Return type is a 2-part that groups the relevant pairs of a 2 part array
+/// (bottom left - top right) and (top left - bottom right) for x shape.
+fn generate_coordinate_pairs_part2(origin: &Coordinate) -> [[Coordinate; 2]; 2] {
+   [
+       // x shape
+       [
+           (origin.0-1, origin.1-1), // top left
+           (origin.0+1, origin.1+1) // bottom right
+       ],
+       [
+           (origin.0+1, origin.1-1), // top right
+           (origin.0-1, origin.1+1) // bottom left
+       ]
+   ]
+}
 
 #[cfg(test)]
 mod tests {
@@ -160,7 +224,7 @@ mod tests {
     fn test_generate_coordinates_north() {
         let origin: Coordinate = (3, 3);
         let direction = Direction::North;
-        let result = generate_coordinates(&origin, &direction);
+        let result = generate_coordinates_part1(&origin, &direction);
         let expected = vec![
             (origin.0, origin.1-1),
             (origin.0, origin.1-2),
@@ -174,7 +238,7 @@ mod tests {
     fn test_generate_coordinates_north_east() {
         let origin: Coordinate = (3, 3);
         let direction = Direction::NorthEast;
-        let result = generate_coordinates(&origin, &direction);
+        let result = generate_coordinates_part1(&origin, &direction);
         let expected = vec![
             (origin.0+1, origin.1-1),
             (origin.0+2, origin.1-2),
@@ -188,7 +252,7 @@ mod tests {
     fn test_generate_coordinates_east() {
         let origin: Coordinate = (3, 3);
         let direction = Direction::East;
-        let result = generate_coordinates(&origin, &direction);
+        let result = generate_coordinates_part1(&origin, &direction);
         let expected = vec![
             (origin.0+1, origin.1),
             (origin.0+2, origin.1),
@@ -202,7 +266,7 @@ mod tests {
     fn test_generate_coordinates_south_east() {
         let origin: Coordinate = (3, 3);
         let direction = Direction::SouthEast;
-        let result = generate_coordinates(&origin, &direction);
+        let result = generate_coordinates_part1(&origin, &direction);
         let expected = vec![
             (origin.0+1, origin.1+1),
             (origin.0+2, origin.1+2),
@@ -216,7 +280,7 @@ mod tests {
     fn test_generate_coordinates_south() {
         let origin: Coordinate = (3, 3);
         let direction = Direction::South;
-        let result = generate_coordinates(&origin, &direction);
+        let result = generate_coordinates_part1(&origin, &direction);
         let expected = vec![
             (origin.0, origin.1+1),
             (origin.0, origin.1+2),
@@ -230,7 +294,7 @@ mod tests {
     fn test_generate_coordinates_south_west() {
         let origin: Coordinate = (3, 3);
         let direction = Direction::SouthWest;
-        let result = generate_coordinates(&origin, &direction);
+        let result = generate_coordinates_part1(&origin, &direction);
         let expected = vec![
             (origin.0-1, origin.1+1),
             (origin.0-2, origin.1+2),
@@ -244,7 +308,7 @@ mod tests {
     fn test_generate_coordinates_west() {
         let origin: Coordinate = (3, 3);
         let direction = Direction::West;
-        let result = generate_coordinates(&origin, &direction);
+        let result = generate_coordinates_part1(&origin, &direction);
         let expected = vec![
             (origin.0-1, origin.1),
             (origin.0-2, origin.1),
@@ -258,7 +322,7 @@ mod tests {
     fn test_generate_coordinates_north_west() {
         let origin: Coordinate = (3, 3);
         let direction = Direction::NorthWest;
-        let result = generate_coordinates(&origin, &direction);
+        let result = generate_coordinates_part1(&origin, &direction);
         let expected = vec![
             (origin.0-1, origin.1-1),
             (origin.0-2, origin.1-2),
@@ -389,7 +453,7 @@ mod tests {
     }
 
     #[test]
-    fn test_finds_multiple_xmas_up() {
+    fn test_finds_multiple_xmas() {
         let input = concat!(
         "S..S\n",
         "A.A.\n",
@@ -401,5 +465,153 @@ mod tests {
         let xmases = d4.how_many_xmas();
 
         assert_eq!(xmases, 3);
+    }
+
+    #[test]
+    fn test_part2_example() {
+        let input = concat!(
+        ".M.S......\n",
+        "..A..MSMS.\n",
+        ".M.S.MAA..\n",
+        "..A.ASMSM.\n",
+        ".M.S.M....\n",
+        "..........\n",
+        "S.S.S.S.S.\n",
+        ".A.A.A.A..\n",
+        "M.M.M.M.M.\n",
+        "..........\n",
+        );
+
+        let d4 = Day04::new(&input);
+        let xmases = d4.how_many_x_mas_part2();
+
+        assert_eq!(xmases, 9);
+    }
+
+    #[test]
+    fn test_part2_simple_plus() {
+        let input = concat!(
+        ".M.\n",
+        "MAS\n",
+        ".S.\n",
+        );
+
+        let d4 = Day04::new(&input);
+        let xmases = d4.how_many_x_mas_part2();
+
+        assert_eq!(xmases, 0);
+    }
+
+    #[test]
+    fn test_part2_inverted_plus() {
+        let input = concat!(
+        ".S.\n",
+        "SAM\n",
+        ".M.\n",
+        );
+
+        let d4 = Day04::new(&input);
+        let xmases = d4.how_many_x_mas_part2();
+
+        assert_eq!(xmases, 0);
+    }
+
+    #[test]
+    fn test_part2_half_inverted_vert_plus() {
+        let input = concat!(
+        ".M.\n",
+        "SAM\n",
+        ".S.\n",
+        );
+
+        let d4 = Day04::new(&input);
+        let xmases = d4.how_many_x_mas_part2();
+
+        assert_eq!(xmases, 0);
+    }
+
+
+    #[test]
+    fn test_part2_half_inverted_hor_plus() {
+        let input = concat!(
+        ".S.\n",
+        "MAS\n",
+        ".M.\n",
+        );
+
+        let d4 = Day04::new(&input);
+        let xmases = d4.how_many_x_mas_part2();
+
+        assert_eq!(xmases, 0);
+    }
+
+    #[test]
+    fn test_part2_simple_ex_plus() {
+        let input = concat!(
+        "MMS\n",
+        "MAS\n",
+        "MSS\n",
+        );
+
+        let d4 = Day04::new(&input);
+        let xmases = d4.how_many_x_mas_part2();
+
+        assert_eq!(xmases, 1);
+    }
+
+    #[test]
+    fn test_part2_simple_ex() {
+        let input = concat!(
+        "M.S\n",
+        ".A.\n",
+        "M.S\n",
+        );
+
+        let d4 = Day04::new(&input);
+        let xmases = d4.how_many_x_mas_part2();
+
+        assert_eq!(xmases, 1);
+    }
+
+    #[test]
+    fn test_part2_simple_inverted_ex() {
+        let input = concat!(
+        "S.M\n",
+        ".A.\n",
+        "M.S\n",
+        );
+
+        let d4 = Day04::new(&input);
+        let xmases = d4.how_many_x_mas_part2();
+
+        assert_eq!(xmases, 0);
+    }
+
+    #[test]
+    fn test_part2_simple_half_inverted_ex() {
+        let input = concat!(
+        "S.M\n",
+        ".A.\n",
+        "S.M\n",
+        );
+
+        let d4 = Day04::new(&input);
+        let xmases = d4.how_many_x_mas_part2();
+
+        assert_eq!(xmases, 1);
+    }
+
+    #[test]
+    fn test_part2_simple_other_half_inverted_ex() {
+        let input = concat!(
+        "M.S\n",
+        ".A.\n",
+        "M.S\n",
+        );
+
+        let d4 = Day04::new(&input);
+        let xmases = d4.how_many_x_mas_part2();
+
+        assert_eq!(xmases, 1);
     }
 }
